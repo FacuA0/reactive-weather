3f3f08d0017c83ae06d6757be0703a3b7a47c7f2
FacuA0 <facundoamarilla28@gmail.com>
2026-03-23 13:09:15 -0300

    Organizando elementos diarios

--- src/components/Day.jsx
+++ src/components/Day.jsx
@@ -3,13 +3,20 @@ import { convertWeatherToHuman, dateToHuman } from "../utils";
 function Day({ day }) {
     console.log("Day", day);
 
+    let maxMin = `${day.temperature_2m_max ?? "-"}°C/${day.temperature_2m_min ?? "-"}°C`;
+
     return (
         <div className="data-item">
             <p><b>{dateToHuman(day.time)}</b></p>
-            <p><b>Cielo:</b> {convertWeatherToHuman(day.weather_code) ?? "-"}</p>
-            <p><b>Temperatura máx:</b> {day.temperature_2m_max ?? "-"}°C</p>
-            <p><b>Temperatura mín:</b> {day.temperature_2m_min ?? "-"}°C</p>
-            <p><b>Probabilidad:</b> {day.precipitation_probability_mean ?? "-"}%</p>
+            <div className="day-icons">
+                <img src="src/assets/icons/default.svg" width={48}/>
+                <img src="src/assets/icons/default.svg" width={48}/>
+            </div>
+            <p>{maxMin}</p>
+            <div className="data-rain">
+                <img src="src/assets/icons/default.svg" width={16} alt="Probabildad de lluvia"/>
+                <p>{day.precipitation_probability_mean ?? "-"}%</p>
+            </div>
         </div>
     );
 }
