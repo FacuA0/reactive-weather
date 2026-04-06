05fd59c2b362e20e49825419a088b27070c253be
FacuA0 <facundoamarilla28@gmail.com>
2026-04-05 21:25:25 -0300

    Cambios de estado y token guardado

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -1,43 +1,79 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 function Location(props) {
     const [error, setError] = useState(null);
-    const [locating, setLocating] = useState(false);
+    const [locating, setLocating] = useState(0);
+    const usernameRef = useRef(null);
 
     function getLocation() {
-        function success(pos) {
+        async function success(pos) {
             setLocating(false);
 
-            let city = {
-                cityName: "No Se, Algun Lado",
-                country_code: "ar",
-                latitude: pos.coords.latitude,
-                longitude: pos.coords.longitude
-            };
+            let latitude = pos.coords.latitude;
+            let longitude = pos.coords.longitude;
+            let language = navigator.language.slice(0, 2);
+            
+            setLocating(2);
 
-            console.log(city);
-
-            props.setCity(city);
+            try {
+                if (usernameRef.current == null) {
+                    usernameRef.current = await userRequest;
+                }
+                let username = usernameRef.current;
+    
+                let query = `lat=${latitude}&lng=${longitude}&lang=${language}&username=${username}`;
+                let cityRequest = await fetch(`http://api.geonames.org/findNearbyPlaceNameJSON?${query}`);
+                let cityJson = (await cityRequest.json()).geonames[0];
+    
+                let city = {
+                    cityName: `${cityJson.name}, ${cityJson.countryName}`,
+                    country_code: cityJson.countryCode,
+                    latitude,
+                    longitude
+                };
+    
+                console.log(city);
+    
+                props.setCity(city);
+            }
+            catch (err) {
+                console.error(err);
+                alert("Error buscando ciudad: " + err);
+                setLocating(0);
+                setError(err);
+            }
         }
-        
+
         function error(err) {
             console.error(err);
             alert(`Hubo un error: ${err}`);
-            setLocating(false);
+            setLocating(0);
             setError(err);
         }
 
         navigator.geolocation.getCurrentPosition(success, error);
-        setLocating(true);
+        let userRequest;
+        if (usernameRef.current == null) {
+            userRequest = fetch("/geotoken.local")
+                .then(res => res.text());
+        }
+
+        setLocating(1);
         setError(null);
     }
+
+    let locatingText = "Usar ubicación";
+    if (locating == 1)
+        locatingText = "Ubicando...";
+    if (locating == 2)
+        locatingText = "Buscando ciudad...";
 
     return (
         <div id="location-box">
             <p>{!error ? "...o usa tu ubicación actual (requiere permisos)" : "Hubo un error: " + error}</p>
             <button onClick={getLocation} disabled={locating}>
                 <img id="logo" width="24" src="src/assets/icons/back.svg"/>
-                {!locating ? "Usar ubicación" : "Ubicando..."}
+                {locatingText}
             </button>
         </div>
     );
